dec507d01faa9b2c66df576349dbd74008ef89c2
Duc Khang Nguyen <duckhang2210@gmail.com>
2020-04-27 21:37:16 -0500

    instance, binding, conditional

---
+++ script.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    product: 'Socks',
+    description: 'These socks are meant to be together!',
+    image: './assets/greensock.jpg',
+    link:
+      'https://www.google.com/search?q=socks&rlz=1C1CHBF_enUS840US840&sxsrf=ALeKk00qNREdJYYFWsMkHwJXWUiIlh1opg:1588033896098&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi1nJKi74npAhUQ7awKHS5pAAoQ_AUoAnoECA8QBA&biw=958&bih=878',
+    inventory: 0,
+    onSale: true
+  }
+});
