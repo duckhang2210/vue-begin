c50bfa39ac1300c56b4e2c4e7dbcf4c48239add5
Duc Khang Nguyen <duckhang2210@gmail.com>
2020-04-27 22:02:03 -0500

    render with v-for

--- script.js
+++ script.js
@@ -7,6 +7,18 @@ var app = new Vue({
     link:
       'https://www.google.com/search?q=socks&rlz=1C1CHBF_enUS840US840&sxsrf=ALeKk00qNREdJYYFWsMkHwJXWUiIlh1opg:1588033896098&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi1nJKi74npAhUQ7awKHS5pAAoQ_AUoAnoECA8QBA&biw=958&bih=878',
     inventory: 0,
-    onSale: true
+    onSale: true,
+    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green'
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue'
+      }
+    ],
+    sizes: ['Small', 'Medium', 'Large']
   }
 });
